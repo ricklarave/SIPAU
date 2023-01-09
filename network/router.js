62495335f58e0a61694563e4f79e11a1e899e8ae
JDari0 <89716225+JDari0@users.noreply.github.com>
2023-01-08 23:57:10 -0500

    Usuario components
    Archivos: controller, interface y storage

--- network/router.js
+++ network/router.js
@@ -1,7 +1,9 @@
 const residente = require('../components/residente/interface')
+const usuario = require('../components/usuario/interface')
 
 const routes = function(server) {
     server.use('/residente', residente)
+    server.use('/usuario', usuario)
 }
 
 module.exports = routes
